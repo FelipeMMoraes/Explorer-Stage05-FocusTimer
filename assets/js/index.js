1deabe7afe0376614dc6fda1e823f5aeaf23e5dc
FelipeMMoraes <felipe.moraes8@hotmail.com>
2022-08-31 17:46:33 -0300

    fix: sound function adjustment and bug fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,13 +48,13 @@ buttonStop.addEventListener('click', function() {
 buttonSoundOff.addEventListener('click', function() {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
-  sound.bgAudio.pause()
+  sound.bgAudio.play()
 })
 
 buttonSoundOn.addEventListener('click', function() {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
-  sound.bgAudio.play()
+  sound.bgAudio.pause()
 })
 
 buttonSet.addEventListener('click', function() {
